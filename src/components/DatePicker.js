8d13104a5ba440ce598aee0a6d44ccfe2275e8dd
alexdru <alexdru@outlook.com>
2020-10-11 18:49:29 +0500

    add locale & custom header

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -3,7 +3,8 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../styles/data-picker/datepicker.scss";
 
-// import { getMonth, getYear } from "date-fns";
+import ru from "date-fns/locale/ru";
+import { getMonth, getYear } from "date-fns";
 
 export default () => {
   const [startDate, setStartDate] = useState(new Date());
@@ -27,15 +28,54 @@ export default () => {
   return (
     <div className="date-picker">
       <DatePicker
+        renderCustomHeader={({
+          date,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => {
+          const months = [
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь",
+          ];
+
+          return (
+            <div className="date-picker__header">
+              <div className="date-picker__header-title">
+                {months[getMonth(date)]} {getYear(date)}
+              </div>
+              <div className="date-picker__header-buttons">
+                <button
+                  className="date-picker__header-arrow-down"
+                  onClick={decreaseMonth}
+                  disabled={prevMonthButtonDisabled}
+                />
+                <button
+                  className="date-picker__header-arrow-up"
+                  onClick={increaseMonth}
+                  disabled={nextMonthButtonDisabled}
+                />
+              </div>
+            </div>
+          );
+        }}
         dateFormat="dd.MM.yyyy"
         selected={startDate}
         onChange={(date) => setStartDate(date)}
         customInput={<CustomInput />}
-        // renderCustomHeader={<CustomHeader />}
-        todayButton="Сегодня "
-        // calendarClassName=""
-        // className=""
-        // calendarClassName=""
+        locale={ru}
+        todayButton="Сегодня"
       />
     </div>
   );
